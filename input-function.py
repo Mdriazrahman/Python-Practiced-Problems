c4d93436d0b0127d0d3f5ddbb1899a11a83b9413
Md. Riaz Rahaman <57906871+Mdriazrahman@users.noreply.github.com>
2024-01-05 00:14:42 +0600

    Create input-function.py

---
+++ input-function.py
@@ -0,0 +1,6 @@
+#Taking string input from console
+#input function always outputs a string. 
+#In order to take input of desired type we have to typecast it to desired data type
+first_name=input("Enter first name:")
+last_name=input("Enter last name:")
+print(f" My name is {first_name} {last_name}")
